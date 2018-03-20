0a9376c9d470765031afea18df1f5b8161cbaf45
mikle <mihailgorbunov74@gmail.com>
2018-03-19 23:50:05 -0700

    index

---
+++ src/main/java/Mikles/homepage/mikle/MikleApplication.java
@@ -0,0 +1,12 @@
+package Mikles.homepage.mikle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikleApplication.class, args);
+	}
+}
